8754e73cd189e3fb4b7b06b7a88bfb645733d743
abartczak629 <a.bartczak.629@studms.ug.edu.pl>
2024-04-08 21:46:24 +0200

    zadanie 3

---
+++ lab06/zad03/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+correct=true
+if ! docker exec frontend ping -c1 -q backend > /dev/null; then
+    echo "Can't connect from frontend to backend"
+    correct=false
+fi
+
+if ! docker exec backend ping -c1 -q database > /dev/null; then
+    echo "Can't connect from backend to database"
+    correct=false
+fi
+
+if docker exec frontend ping -c1 -q database > /dev/null 2>&1; then
+    echo "Can connect from frontend to database"
+    correct=false
+fi
+
+$correct && echo "Everything works correctly"
